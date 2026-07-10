b61ed06ae54916a787d04fb854f00ee89326f1ce
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-07-10 17:59:37 +0300

    Refactor widget test to use new main.dart path
    Updated import path for main.dart to match new project structure.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_first_app/main.dart';
+import 'package:scanner_for_android/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
